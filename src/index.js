e5751a14b938b75fb41e5b4de96cb69f214b2bff
Akashms45 <akashms452002@gmail.com>
2025-12-23 22:30:07 +0530

    deploy e-com app

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { RegisterProvider } from "./context/RegContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/e-com_websit">
       <LoginProvider>
         <CartProvider>
           <RegisterProvider>
